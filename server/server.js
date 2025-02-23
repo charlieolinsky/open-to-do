53fa90309fd7bf9eb77ff280c09825d56734e69b
Charles Olinsky <charlesolinsky@gmail.com>
2025-02-23 15:50:14 -0500

    sqlite init

--- server/server.js
+++ server/server.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
 
+//Middleware? JSON? CORS?
+
+//Setup Sqlite DB
+const sqlite3 = require('sqlite3').verbose() //verbose gives extra err details
+const db = new sqlite3.Database('./tasks.db', (err) => {
+    if (err) console.error('DB Error: ', err);
+    else console.log('SQLite connected');
+})
+
+db.run(`CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT, 
+    isComplete INTEGER DEFAULT 0,
+    dueDate TEXT
+)`)
+
+//Setup Webserver
 const port = 8080
 
 app.get("/", (req, res) => {
@@ -8,8 +26,6 @@ app.get("/", (req, res) => {
 })
 
 app.listen(port, (err) => {
-    if (err === undefined)
-        console.log("Server connected to port " + port)
-    else
-        console.error("Server failed to connect to port " + port)
+    if (!err) console.log("Server connected to port " + port)
+    else console.error("Server failed to connect to port " + port)
 })
